af84829b6e8de4aad0c5ac7a47cfd692088bc983
carmelo <ciccionevagante@gmail.com>
2025-05-09 10:41:22 +0200

    Implementazione della skill per restituire l'orario

---
+++ src/main/java/it/aichallenge/skills/SkillTime.java
@@ -0,0 +1,21 @@
+package it.aichallenge.skills;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class SkillTime implements BotSkill{
+
+   @Override
+   public String tryReply(String userMessage){
+       if (userMessage == null)return null;
+       String lower =userMessage.toLowerCase();
+       if(lower.contains("che ore sono")||lower.contains("orario") || lower.contains("che ora è")){
+           LocalTime now =LocalTime.now();
+           DateTimeFormatter formatter= DateTimeFormatter.ofPattern("HH:mm:ss");
+
+           return "sono le"+ now.format(formatter);
+       }
+        return null;
+   }
+}
+
