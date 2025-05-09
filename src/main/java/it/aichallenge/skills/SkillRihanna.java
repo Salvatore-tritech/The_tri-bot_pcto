499148635e2a4881ea230ae7d02392f7113c69ac
drago <dragomatteo19@gmail.com>
2025-05-09 14:44:14 +0200

    classe SkillRihanna (iniziale)

---
+++ src/main/java/it/aichallenge/skills/SkillRihanna.java
@@ -0,0 +1,18 @@
+
+package it.aichallenge.skills;
+
+public class SkillRihanna implements BotSkill{
+
+    @Override
+    public String tryReply(String userMessage) {
+        if (userMessage == null) return null;
+
+        String upper= userMessage.toUpperCase();
+        if (upper.equals("/RIHANNA")) {
+            return "Ciao, sono Rihanna! ora converserò io con te...";
+        }
+
+        return null;
+
+    }
+}
