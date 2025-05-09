0c1d3aaba4f850d716cca51d39c4afe735061a84
GiulioFinocchiaro <giulfinocchiaro@icloud.com>
2025-05-09 11:03:56 +0200

    Aggiunto skill Time - aggiornamento

---
+++ src/main/java/it/aichallenge/skills/SkillTime.java
@@ -0,0 +1,55 @@
+package it.aichallenge.skills;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.text.DecimalFormat;
+import java.time.LocalTime;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SkillTime implements BotSkill {
+
+    public static String extractZone(String userMessage) {
+        if (userMessage.contains("zona:")){
+            int size = userMessage.indexOf("zona:");
+            return userMessage.substring(size+"zona:".length(), userMessage.length()-1);
+        }
+        return null;
+    }
+
+    @Override
+    public String tryReply(String userMessage){
+        if (userMessage.toLowerCase().contains("ora") || userMessage.toLowerCase().contains("orario") || userMessage.toLowerCase().contains("ore")) {
+            if (userMessage.toLowerCase().contains("qual'è") || userMessage.toLowerCase().contains("che")){
+                String fuso = extractZone(userMessage);
+                if (fuso == null) {
+                    DecimalFormat df = new DecimalFormat("#.00");
+                    return "L'orario attuale è " + LocalTime.now();
+                }
+                else {
+                    HttpRequest richiesta = HttpRequest.newBuilder(
+                                    URI.create("https://timeapi.io/api/time/current/zone?timeZone=" + fuso))
+                            .header("Content-Type", "application/json")
+                            .GET()
+                            .build();
+                    try {
+                        HttpResponse<String> risposta = HttpClient.newHttpClient().send(richiesta, HttpResponse.BodyHandlers.ofString());
+                        JSONObject json = new JSONObject(risposta.body());
+                         return "L'orario attuale per il fuso orario "+fuso+" è "+ json.getString("time");
+                    } catch (IOException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
